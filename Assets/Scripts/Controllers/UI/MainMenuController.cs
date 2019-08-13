f518590e0da3c8e4f92620d6da9b54818e3cb00a
Zavier Divelbiss <semiviral@gmail.com>
2019-08-13 12:50:27 -0700

    version text

--- Assets/Scripts/Controllers/UI/MainMenuController.cs
+++ Assets/Scripts/Controllers/UI/MainMenuController.cs
@@ -11,12 +11,14 @@ namespace Controllers.UI
 {
     public class MainMenuController : MonoBehaviour
     {
+        public Text VersionText;
         public Button ExitButton;
         public Button PlayButton;
 
         // Start is called before the first frame update
         private void Start()
         {
+            VersionText.text = $"v{Application.version}";
             ExitButton.onClick.AddListener(() => StaticMethods.ApplicationClose());
             PlayButton.onClick.AddListener(PlayGame);
         }
