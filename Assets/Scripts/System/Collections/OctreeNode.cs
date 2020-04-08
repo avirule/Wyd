bdd9d5ea558fe3c5d8edadb356db76fc8e84947b
Zavier Divelbiss <semiviral@gmail.com>
2020-04-08 01:18:03 -0500

    Update OctreeNode.cs

--- Assets/Scripts/System/Collections/OctreeNode.cs
+++ Assets/Scripts/System/Collections/OctreeNode.cs
@@ -1,6 +1,7 @@
 #region
 
 using System;
+using System.Collections.Generic;
 using Unity.Mathematics;
 
 #endregion
@@ -22,17 +23,19 @@ namespace Wyd.System.Collections
             new float3(1, 1, 1)
         };
 
-        private OctreeNode<T>[] _Nodes;
+
+        private readonly List<OctreeNode<T>> _Nodes;
 
         public Volume Volume { get; }
         public T Value { get; private set; }
-        public bool IsUniform { get; private set; }
+        public bool IsUniform => _Nodes.Count == 0;
 
-        public OctreeNode(float3 centerPoint, float extent, T value)
+        public OctreeNode(float3 centerPoint, float size, T value)
         {
-            Volume = new Volume(centerPoint, new float3(extent));
+            _Nodes = new List<OctreeNode<T>>();
+
+            Volume = new Volume(centerPoint, new float3(size));
             Value = value;
-            IsUniform = true;
         }
 
         public void Collapse()
@@ -43,8 +46,7 @@ namespace Wyd.System.Collections
             }
 
             Value = _Nodes[0].Value;
-            _Nodes = null;
-            IsUniform = true;
+            _Nodes.Clear();
         }
 
         public void Populate()
@@ -52,19 +54,31 @@ namespace Wyd.System.Collections
             float centerOffsetValue = Volume.Extents.x / 2f;
             float3 centerOffset = new float3(centerOffsetValue);
 
-            _Nodes = new[]
-            {
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[0] * centerOffset), Volume.Extents.x, Value),
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[1] * centerOffset), Volume.Extents.x, Value),
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[2] * centerOffset), Volume.Extents.x, Value),
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[3] * centerOffset), Volume.Extents.x, Value),
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[4] * centerOffset), Volume.Extents.x, Value),
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[5] * centerOffset), Volume.Extents.x, Value),
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[6] * centerOffset), Volume.Extents.x, Value),
-                new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[7] * centerOffset), Volume.Extents.x, Value)
-            };
+            _Nodes.Clear();
 
-            IsUniform = false;
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[0] * centerOffset),
+                Volume.Extents.x, Value));
+
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[1] * centerOffset),
+                Volume.Extents.x, Value));
+
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[2] * centerOffset),
+                Volume.Extents.x, Value));
+
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[3] * centerOffset),
+                Volume.Extents.x, Value));
+
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[4] * centerOffset),
+                Volume.Extents.x, Value));
+
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[5] * centerOffset),
+                Volume.Extents.x, Value));
+
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[6] * centerOffset),
+                Volume.Extents.x, Value));
+
+            _Nodes.Add(new OctreeNode<T>(Volume.CenterPoint + (_Coordinates[7] * centerOffset),
+                Volume.Extents.x, Value));
         }
 
         public T GetPoint(float3 point)
@@ -139,6 +153,14 @@ namespace Wyd.System.Collections
             }
 
             return true;
+        }
+
+        public IEnumerable<T> GetAllData()
+        {
+            for (int index = 0; index < WydMath.Product(Volume.Size); index++)
+            {
+                yield return GetPoint(Volume.MinPoint + WydMath.IndexTo3D(index, WydMath.ToInt(Volume.Size)));
+            }
         }
 
 
